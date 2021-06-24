08b961d3aca170cda2bddf30d4d5770c45068314
Wachayathorn Singsena <wachayathorn.s@rvconnex.com>
2021-06-24 13:05:15 +0700

    feat: update

--- dashboard/dashboard_deployment.sh
+++ dashboard/dashboard_deployment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.3.1/aio/deploy/recommended.yaml
 kubectl apply -f ./kubernetes_dashboard_user.yaml
 kubectl apply -f ./kubernetes_dashboard_role.yaml
-kubectl apply -f ./kubernetes_dashboard._ingressyaml
+kubectl apply -f ./kubernetes_dashboard_ingress.yaml
